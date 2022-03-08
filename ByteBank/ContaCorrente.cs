c24c2895a10ea70bb322e45a8a1e7f8e7f32394e
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-03-08 14:52:30 -0300

    Primeiro método

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -4,5 +4,18 @@
     public int agencia;
     public int numero;
     public double saldo;
+
+    public bool Sacar(double valor)
+    {
+
+        if(this.saldo < valor)
+        {
+            return false;
+        } else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
     
 }
